665c5e2eef5c6c9bf152403973132a1c28710b1e
robin <106778@student.drenthecollege.nl>
2019-05-14 13:35:47 +0200

    opdracht 1 verbeterd met een recursieve methode

--- src/nl/drenthecollege/modules/a2/eindopdrachten/Opdracht1.java
+++ src/nl/drenthecollege/modules/a2/eindopdrachten/Opdracht1.java
@@ -5,23 +5,46 @@
  */
 package nl.drenthecollege.modules.a2.eindopdrachten;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Erik Mast <e.mast@drenthecollege.nl>
+ * @author Robin Brokmann 106778@student.drenthecollege.nl
  */
 public class Opdracht1 {
     //Fibonacci reeks is 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
     public static void main(String[] args) {
-        System.out.println("De Fibonacci reeks is 0, 1, 1, 2, 3, 5, 8, 13, 21, 34");
-        System.out.println("Element 0 = 0");
-        System.out.println("Element 1 = 1");
-        System.out.println("Element 2 = 1");
-        System.out.println("Element 3 = 2");
-        System.out.println("Element 4 = 3");
-        System.out.println("Element 5 = 5");
-        System.out.println("Element 6 = 9");
-        System.out.println("Element 7 = 14");
-        System.out.println("Element 8 = 23");
-        System.out.println("Element 9 = 37");
-    }    
+
+        final int nummer = 10;
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("De fibonacci reeks is");
+
+        List<String> elementlist = new ArrayList<>();
+
+
+        for (int counter = 0; counter < nummer; counter++){
+            builder.append(fibonacciBerekenen(counter));
+            if (counter != nummer  - 1){
+                builder.append(",");
+            }
+            elementlist.add("Element " + counter + " is: " + fibonacciBerekenen(counter));
+
+        }
+        System.out.println(builder.toString());
+
+        for(String s : elementlist){
+            System.out.println(s);
+        }
+    }
+
+    private static int fibonacciBerekenen(int nummer){
+        if(nummer < 2){
+            return nummer;
+        } else {
+            return (fibonacciBerekenen(nummer - 1) + fibonacciBerekenen(nummer - 2));
+        }
+    }
 }
